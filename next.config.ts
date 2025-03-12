fc28f5ff815f082a65f4099b49948bdaa1f0af07
Gebriel090 <74470175+Gebriel090@users.noreply.github.com>
2025-03-12 15:41:19 -0300

    correcoes project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,51 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  poweredByHeader: false,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "photos.app.goo.gl",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Aplique esses headers em todas as rotas da API
+        headers: [
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "no-referrer" },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self'; object-src 'none'; img-src 'self' lh3.googleusercontent.com i.ibb.co res.cloudinary.com photos.app.goo.gl;",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
